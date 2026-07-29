b216696725d85822bba40555bd8f153eedb7b7f4
ffish54 <leo.bovey05@gmail.com>
2026-07-29 17:39:47 +0100

    fixed burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,37 @@
 (function () {
-    emailjs.init({
-        publicKey: "wvojyQ6MBVoljP_lT",
-    });
+  emailjs.init({
+    publicKey: "wvojyQ6MBVoljP_lT",
+  });
 })();
 
 const menuBtn = document.getElementById("menu-btn");
 const navMenu = document.getElementById("nav-menu");
 
-menuBtn.addEventListener("click", function() {
-    navMenu.classList.toggle("show");
+menuBtn.addEventListener("click", function () {
+  navMenu.classList.toggle("show");
 });
 
 const links = document.querySelectorAll("#nav-menu a");
 
-links.forEach(link => {
-
-    link.addEventListener("click", () => {
-
-        navMenu.classList.remove("show");
-
-    });
-
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show");
+  });
 });
 
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    emailjs.sendForm(
-        "service_0tkdj4o",
-        "template_0cm617l",
-        this
-    )
-    .then(() => {
+    emailjs
+      .sendForm("service_0tkdj4o", "template_0cm617l", this)
+      .then(() => {
         alert("Enquiry sent successfully!");
         this.reset();
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
         alert("Failed to send enquiry.");
-    });
-
-});
+      });
+  });
